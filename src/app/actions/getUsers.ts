9576e468754eb7d04e18e43cefa837d1da6ecb34
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-20 09:29:11 +0100

    add no cache

--- src/app/actions/getUsers.ts
+++ src/app/actions/getUsers.ts
@@ -3,7 +3,9 @@ import { Config } from "../../../Config";
 
 export const getUsers = async () => {
   try {
-    let response = await fetch(`${Config.baseUrl}/users`);
+    let response = await fetch(`${Config.baseUrl}/users`, {
+      cache: "no-cache",
+    });
     let data = await response.json();
     return data;
   } catch (error) {
