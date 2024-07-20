a72d11ef9bdf5072aee9783075cc012e409983dd
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-20 09:29:00 +0100

    created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import UsersList from "./components/UsersList";
+import UsersPage from "./users/page";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UsersList />
+      <UsersPage />
     </main>
   );
 }
