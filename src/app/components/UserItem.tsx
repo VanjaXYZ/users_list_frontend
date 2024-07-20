99f07f0690d657402b6d14460ef16e3dd1df32e1
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-20 13:11:27 +0100

    created

--- src/app/components/UserItem.tsx
+++ src/app/components/UserItem.tsx
@@ -1,8 +1,20 @@
+"use client";
 import React from "react";
 import { User } from "../types/userType";
+import { handleDeleteUser } from "../actions/deleteUser";
 
 const UserItem = ({ user }: { user: User }) => {
-  return <li key={user.id}>{user.name}</li>;
+  return (
+    <li key={user.id} className="w-full flex justify-between py-2">
+      {user.name}{" "}
+      <button
+        className="bg-white/20 px-2 py-1 hover:bg-white/40"
+        onClick={() => handleDeleteUser(user.id)}
+      >
+        Remove
+      </button>
+    </li>
+  );
 };
 
 export default UserItem;
