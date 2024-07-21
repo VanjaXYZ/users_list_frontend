664fde3a348300a37d5c5bc1857a54088ff69adb
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-21 16:02:44 +0100

    add more data

--- src/app/components/UserItem.tsx
+++ src/app/components/UserItem.tsx
@@ -9,12 +9,18 @@ const UserItem = ({ user }: { user: User }) => {
   const [name, setName] = useState(user.name);
   const [email, setEmail] = useState(user.email);
   const [age, setAge] = useState(user.age);
+  const [job, setJob] = useState(user.job);
+  const [workExperience, setWorkExperience] = useState(user.work_experience);
+  const [hobbies, setHobbies] = useState(user.hobbies);
 
   const handleUpdate = async () => {
     const formData = new FormData();
     formData.append("name", name);
     formData.append("email", email);
     formData.append("age", age.toString());
+    formData.append("job", job);
+    formData.append("work_experience", workExperience.toString());
+    // formData.append("hobbies", hobbies);
     await handleUpdateUser(user.id, formData);
     setIsEditing(false);
   };
@@ -28,18 +34,44 @@ const UserItem = ({ user }: { user: User }) => {
             value={name}
             onChange={(e) => setName(e.target.value)}
             className="px-2 py-1 text-black"
+            placeholder="Name"
           />
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             className="px-2 py-1 text-black"
+            placeholder="Email"
           />
           <input
             type="number"
             value={age}
             onChange={(e) => setAge(Number(e.target.value))}
             className="px-2 py-1 text-black"
+            placeholder="Age"
+          />
+          <input
+            type="text"
+            value={job}
+            onChange={(e) => setJob(e.target.value)}
+            className="px-2 py-1 text-black"
+            placeholder="Current job"
+          />
+          <input
+            type="number"
+            value={workExperience}
+            onChange={(e) => setWorkExperience(Number(e.target.value))}
+            className="px-2 py-1 text-black"
+            placeholder="Work experience"
+          />
+          <input
+            type="text"
+            value={hobbies}
+            onChange={(e) =>
+              setHobbies((prev: string[]) => [...prev, e.target.value])
+            }
+            className="px-2 py-1 text-black"
+            placeholder="Hobbies"
           />
           <div className="flex justify-end gap-2">
             <button
@@ -58,8 +90,29 @@ const UserItem = ({ user }: { user: User }) => {
         </div>
       ) : (
         <>
-          <span className="">Name: {user.name}</span>
-          <span className="font-light">E-mail: {user.email}</span>
+          <div className="flex">
+            <span className="w-[200px]">Name:</span>
+            <span>{user.name}</span>
+          </div>
+          <div className="flex">
+            <span className="w-[200px]">Email:</span>
+            <span>{user.email}</span>
+          </div>
+          <div className="flex">
+            <span className="w-[200px]">Age:</span>
+            <span>{user.age}</span>
+          </div>
+          <div className="flex">
+            <span className="w-[200px]">Current job:</span>
+            <span>{user.job}</span>
+          </div>
+          <div className="flex">
+            <span className="w-[200px]">Work experience:</span>
+            <span>
+              {user.work_experience}{" "}
+              {`${user.work_experience < 1 ? "year" : "years"}`}
+            </span>
+          </div>
           <div className="flex gap-2 justify-end py-2">
             <button
               className="bg-white/20 px-2 rounded hover:bg-white/40"
