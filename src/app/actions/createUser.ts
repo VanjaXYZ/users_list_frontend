6f103e230c39c94a8486b42d581f409a45ec668e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-21 15:01:15 +0100

    fix

--- src/app/actions/createUser.ts
+++ src/app/actions/createUser.ts
@@ -4,20 +4,30 @@ import { Config } from "../../../Config";
 
 export const handleSubmit = async (formData: FormData) => {
   try {
+    const data = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      age: parseInt(formData.get("age") as string), // Uveri se da age bude broj
+    };
+
     let response = await fetch(`${Config.baseUrl}/users`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        name: formData.get("name"),
-        email: formData.get("email"),
-        age: formData.get("age"),
-      }),
+      body: JSON.stringify(data),
     });
+
     revalidatePath("/users");
-    return await response.json();
-  } catch (error) {
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(result.message || "Something went wrong");
+    }
+    console.log(result);
+    return result;
+  } catch (error: any) {
     console.error(error);
+    throw new Error(error.message);
   }
 };
