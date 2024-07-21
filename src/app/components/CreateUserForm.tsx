4090269844c409eecf5e9d8fa10913687753269e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-21 21:53:25 +0100

    add borders

--- src/app/components/CreateUserForm.tsx
+++ src/app/components/CreateUserForm.tsx
@@ -9,50 +9,57 @@ const CreateUserForm = () => {
     ref.current.reset();
   };
   return (
-    <form ref={ref} className="grid grid-cols-2 gap-4" action={onSubmit}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        className="px-2 py-1 text-black"
-        autoComplete="on"
-      />
-      <input
-        type="text"
-        name="email"
-        placeholder="Email"
-        className="px-2 py-1 text-black"
-        autoComplete="on"
-      />
-      <input
-        type="number"
-        name="age"
-        placeholder="Age"
-        className="px-2 py-1 text-black"
-        autoComplete="on"
-      />
-      <input
-        type="text"
-        name="job"
-        placeholder="Current job"
-        className="px-2 py-1 text-black"
-      />
-      <input
-        type="number"
-        name="work_experience"
-        placeholder="Work experience"
-        className="px-2 py-1 text-black"
-        autoComplete="on"
-      />
-      <input
-        type="text"
-        name="hobbies"
-        placeholder="Hobbies"
-        className="px-2 py-1 text-black"
-      />
-      <button type="submit" className="bg-white/20 rounded hover:bg-white/40">
-        Submit
-      </button>
+    <form ref={ref} action={onSubmit}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="px-2 py-1 text-black border rounded"
+          autoComplete="on"
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          className="px-2 py-1 text-black border rounded"
+          autoComplete="on"
+        />
+        <input
+          type="number"
+          name="age"
+          placeholder="Age"
+          className="px-2 py-1 text-black border rounded"
+          autoComplete="on"
+        />
+        <input
+          type="text"
+          name="job"
+          placeholder="Current job"
+          className="px-2 py-1 text-black border rounded"
+        />
+        <input
+          type="number"
+          name="work_experience"
+          placeholder="Work experience"
+          className="px-2 py-1 text-black border rounded"
+          autoComplete="on"
+        />
+        <input
+          type="text"
+          name="hobbies"
+          placeholder="Hobbies"
+          className="px-2 py-1 text-black border rounded"
+        />
+      </div>
+      <div className="py-2">
+        <button
+          type="submit"
+          className="border rounded bg-teal-700 text-white font-bold uppercase tracking-widest px-4 py-1 w-full"
+        >
+          Submit
+        </button>
+      </div>
     </form>
   );
 };
