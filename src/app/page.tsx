ed9c107f2ff43ae4ca32cda5f3a13337138439b9
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-21 22:47:53 +0100

    add welcomescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import UsersPage from "./users/page";
+import WelcomeScreen from "./components/(welcome-screen)/WelcomeScreen";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UsersPage />
+      <WelcomeScreen />
     </main>
   );
 }
