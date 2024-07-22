8e8b808b73fe349cdab994719b1b6b1254f2aa08
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-22 20:12:51 +0100

    created signup UI

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,3 +1,4 @@
+import SignupForm from "@/app/components/(auth)/SignupForm";
 import { Metadata } from "next";
 import React from "react";
 
@@ -7,22 +8,9 @@ export const metadata: Metadata = {
 
 const Page = () => {
   return (
-    <form>
-      <h3>Welcome</h3>
-      <div>
-        <input type="text" name="name" placeholder="Username" />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="number" name="age" placeholder="Age" />
-        <input type="text" name="job" placeholder="Current job" />
-        <input
-          type="text"
-          name="work_experience"
-          placeholder="Work experience"
-        />
-        <input type="text" name="hobbies" placeholder="Hobbies" />
-        <button type="submit">Signup</button>
-      </div>
-    </form>
+    <>
+      <SignupForm />
+    </>
   );
 };
 
---
+++ src/app/components/(auth)/SignupForm.tsx
@@ -0,0 +1,73 @@
+import { LockKeyhole } from "lucide-react";
+import React from "react";
+
+const SignupForm = () => {
+  return (
+    <form className="w-1/3 m-auto h-96 mt-20 py-2">
+      <div className="flex justify-center flex-col items-center gap-4">
+        <LockKeyhole size={48} />
+        <h3 className="text-center text-2xl uppercase antialiased font-semibold">
+          Welcome
+        </h3>
+      </div>
+      <div className="grid grid-cols-2 gap-2 py-4">
+        <input
+          type="text"
+          placeholder="Full name"
+          className="px-2 py-1 text-black border rounded"
+          name="name"
+          autoComplete="on"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="px-2 py-1 text-black border rounded"
+          name="password"
+          autoComplete="on"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          className="px-2 py-1 text-black border rounded"
+          name="email"
+          autoComplete="on"
+          required
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          className="px-2 py-1 text-black border rounded"
+          name="age"
+          autoComplete="on"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Job"
+          className="px-2 py-1 text-black border rounded"
+          name="job"
+          autoComplete="on"
+          required
+        />
+        <input
+          type="number"
+          placeholder="Work experience"
+          className="px-2 py-1 text-black border rounded"
+          name="work_experience"
+          autoComplete="on"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="border rounded bg-teal-700 text-white font-bold uppercase tracking-widest px-4 py-1 hover:bg-teal-500 w-full"
+      >
+        Signup
+      </button>
+    </form>
+  );
+};
+
+export default SignupForm;
