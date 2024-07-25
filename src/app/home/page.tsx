7579eab704e1c77c68df093da2d4aa469bb98bf1
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-25 14:24:31 +0100

    add video

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const Page = () => {
-  return <div>Hello from Home page...</div>;
+  return (
+    <>
+      <div className="min-h-screen relative">
+        <video
+          src="https://videos.pexels.com/video-files/3209298/3209298-uhd_2560_1440_25fps.mp4"
+          autoPlay
+          muted
+          loop
+          className="absolute top-0 -z-10 w-full h-full object-cover"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Page;
