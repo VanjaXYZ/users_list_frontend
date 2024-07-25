e89401c02ad6076df1d074aaf92187fab90f3ad9
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-25 01:25:56 +0100

    add sort query

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -15,11 +15,13 @@ const UsersPage = async ({
     query?: string;
     filter?: string;
     page?: string;
+    sort?: string;
   };
 }) => {
   const query = searchParams?.query || "";
   const filterParam = searchParams?.filter || "name"; // if there is no filter chosen add 'name' value to it as default
   const currentPage = Number(searchParams?.page) || 1;
+  const sortBy = searchParams?.sort || "";
 
   return (
     <div className="flex flex-col p-12 gap-8 w-full">
@@ -30,6 +32,7 @@ const UsersPage = async ({
           query={query}
           filter={filterParam}
           currentPage={currentPage}
+          sortBy={sortBy}
         />
       </Suspense>
     </div>
